7108f468b827927510055df8b50b9d01b8780c00
Leang <leang@groundcontrol.(none)>
2015-04-30 13:34:45 -0600

    updated to follow a square

--- src/waypoint_generator.cpp
+++ src/waypoint_generator.cpp
@@ -32,15 +32,21 @@ int main(int argc, char** argv)
     std::vector<geometry_msgs::Vector3> desired_positions;
     
     fillPositionList(desired_positions);
-    double bound = 0.2;
+    double bound = 0.025;
     des_pos_out = desired_positions[0];
     int max = desired_positions.size()-1;
     int arg = 0;
+    int count = 0;
+    int countBound = 20;
     while(ros::ok())
     {
-    
         if(normSquared(des_pos_out,curr_pos)<bound*bound)
         {
+            count++;
+        }
+        if(count > countBound)
+        {
+            count = 0;
             if(arg < max)
                 arg++;
             else
@@ -50,7 +56,6 @@ int main(int argc, char** argv)
             //ROS_INFO("Des Pos: %f, %f, %f",des_pos_out.x,des_pos_out.y,des_pos_out.z);
         }
        
-        
         des_pos_pub.publish(des_pos_out);
         ros::spinOnce();
         loop_rate.sleep();
@@ -60,27 +65,49 @@ int main(int argc, char** argv)
 void fillPositionList(std::vector<geometry_msgs::Vector3>& posList)
 {
     geometry_msgs::Vector3 left, right, front, back, top, bottom, middle;
-
-    double del = 0.7;
+    geometry_msgs::Vector3 top_left, top_right, bot_left, bot_right;
+    double del = 0.25;
     double xCen,yCen,zCen;
     //xCen = 1.21; yCen = -1.3; zCen = 1.2;
     //xCen = 0.0; yCen = 0.0; zCen = 0.3; // Andy crazy flie stuff
-    xCen = 0.5; yCen = -1.0; zCen = 0.5;
+    xCen = 0.5; yCen = -1.1; zCen = 0.5;
     middle.x = xCen;       middle.y = yCen;       middle.z = zCen;
-    left.x   = xCen;       left.y   = yCen+del;   left.z   = zCen;
+    /*left.x   = xCen;       left.y   = yCen+del;   left.z   = zCen;
     right.x  = xCen;       right.y  = yCen - del; right.z  = zCen;
     front.x  = xCen + del; front.y  = yCen;       front.z  = zCen;
     back.x   = xCen - del; back.y   = yCen;       back.z   = zCen;
     top.x    = xCen;       top.y    = yCen;       top.z    = zCen + del;
     bottom.x = xCen;       bottom.y = yCen;       bottom.z = zCen - del;
     
-    /*posList.push_back(left);
-    posList.push_back(front);
+    posList.push_back(left);
+    //posList.push_back(front);
     posList.push_back(right);
-    posList.push_back(back);
+    /*posList.push_back(back);
     posList.push_back(top);
     posList.push_back(bottom);*/
     posList.push_back(middle);
+    
+    /*top_left.x  = xCen + del;     top_left.y = yCen + 2.0*del;     top_left.z = zCen;
+    top_right.x = xCen + del;    top_right.y = yCen - 2.0*del;    top_right.z = zCen;
+    bot_left.x  = xCen - del;     bot_left.y = yCen + 2.0*del;     bot_left.z = zCen;
+    bot_right.x = xCen - del;    bot_right.y = yCen - 2.0*del;    bot_right.z = zCen;
+    
+    posList.push_back(bot_left);
+    posList.push_back(top_left);
+    posList.push_back(top_right);
+    posList.push_back(bot_right);
+    posList.push_back(bot_left);*/
+    
+    /*top_left.x = xCen + del;     top_left.y = yCen + del;      top_left.z = zCen + del;
+    top_right.x = xCen + del;    top_right.y = yCen - del;    top_right.z = zCen + del;
+    bot_left.x = xCen - del;     bot_left.y = yCen + del;      bot_left.z = zCen + del;
+    bot_right.x = xCen - del;    bot_right.y = yCen - del;    bot_right.z = zCen + del;
+    
+    posList.push_back(bot_left);
+    posList.push_back(bot_right);
+    posList.push_back(top_right);
+    posList.push_back(top_left);
+    posList.push_back(bot_left);*/
     
 }
 
